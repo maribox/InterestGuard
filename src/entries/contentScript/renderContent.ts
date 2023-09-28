cd7823088c83de4dd9fb3b012cb25b5224cf063c
MySurmise <mearbos@gmail.com>
2023-09-29 01:58:00 +0200

    first attempt interacting with video elements on page

--- src/entries/contentScript/renderContent.ts
+++ src/entries/contentScript/renderContent.ts
@@ -30,3 +30,24 @@ export default async function renderContent(
 
   render(appRoot);
 }
+
+
+// first code concept (working in chrome devtools)
+let contents = document.querySelectorAll("#content");
+let counter = 0;
+for (let content of contents) {
+  try {
+    if (Array.from(content.children[0].children).some((child) => child.id === "dismissible") && content.querySelectorAll("#dismissible").length == 1) {
+      let dismissible = content.querySelectorAll("#dismissible")[0];
+      let menu = dismissible.querySelectorAll("#menu")[0];
+      console.log(dismissible.querySelectorAll("#video-title")[0].innerText);
+      console.log(menu.querySelectorAll('button[id="button"]')[0].querySelectorAll("div")[0].click());
+      counter++;
+      break;
+    }
+  } catch {
+    continue;
+  }
+}
+let popupoptions = document.getElementsByTagName("ytd-popup-container")[0].getElementsByTagName("tp-yt-paper-listbox")[0].children;
+popupoptions[popupoptions.length - 3].click();
